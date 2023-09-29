bb0ca9108749e5a74d89cf78a6ed48dabdfdb7bf
Cameron McDaniel <143523058+cameronislame@users.noreply.github.com>
2023-09-29 15:18:02 -0700

    Update cmcdaniel.h

--- cmcdaniel.h
+++ cmcdaniel.h
@@ -2,10 +2,10 @@
 #define _Cameron_M_
 
 //Cameron's project files
-//
 
-extern void display_name(int, int);
-extern void display_border(int xres, int yres);
+
+//extern void display_name(int, int);
+//extern void display_border(int xres, int yres);
 extern void display_credits(int xres, int yres);
 
 class cameron{
