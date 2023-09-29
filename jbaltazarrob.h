e0a24bb10d976ca3d2edd60c120078a1e75956e8
JesusAstro <115951299+JesusAstro@users.noreply.github.com>
2023-09-29 09:54:08 -0700

    Create jbaltazarrob.h

---
+++ jbaltazarrob.h
@@ -0,0 +1,11 @@
+#include "jbaltazarrob.h"
+#ifndef _JESUS_H_
+#define _JESUS_H_
+/**
+ * Author:    Jesus Baltazar Robles
+ * Created:   9/29/2023
+ * 
+ **/
+/*Header files that will define level design and objects implementation*/
+
+#endif
