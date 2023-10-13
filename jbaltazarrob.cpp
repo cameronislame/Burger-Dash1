e538369b4868187c1b5bfdeb09e9702011e65c95
JesusAstro <115951299+JesusAstro@users.noreply.github.com>
2023-10-12 23:46:32 -0700

    Add files via upload

--- jbaltazarrob.cpp
+++ jbaltazarrob.cpp
@@ -1,5 +1,54 @@
 #include "jbaltazarrob.h"
+#include <iostream>
+#include <cstddef>
+#include <cstring>
+#include <cstdio>
 /**
 * Auhtor: Jesus Baltazar Robles
 * Date: 9/29/2023
 */
+/*LEVEL CLASS INITIALIZATION
+ * CODE COMES FROM GORDON WALK2*/
+Level::Level() {
+        //Log("Level constructor\n");
+        tilesize[0] = 32;
+        tilesize[1] = 32;
+        ftsz[0] = (Flt)tilesize[0];
+        ftsz[1] = (Flt)tilesize[1];
+        tile_base = 220.0;
+        //read level
+        FILE *fpi = fopen("lvl.txt","r");
+        if (fpi) {
+        nrows=0;
+        char line[100];
+        while (fgets(line, 100, fpi) != NULL) {
+            removeCrLf(line);
+            int slen = strlen(line);
+            ncols = slen;
+            //Log("line: %s\n", line);
+            for (int j=0; j<slen; j++) {
+            arr[nrows][j] = line[j];
+            }
+            ++nrows;
+        }
+        fclose(fpi);
+        //printf("nrows of background data: %i\n", nrows);
+        }
+        for (int i=0; i<nrows; i++) {
+        for (int j=0; j<ncols; j++) {
+            printf("%c", arr[i][j]);
+        }
+        printf("\n");
+        }
+    }
+void Level::removeCrLf(char *str) {
+        //remove carriage return and linefeed from a Cstring
+        char *p = str;
+        while (*p) {
+        if (*p == 10 || *p == 13) {
+            *p = '\0';
+            break;
+        }
+        ++p;
+        }
+}
--- jbaltazarrob.h
+++ jbaltazarrob.h
@@ -6,5 +6,16 @@
  * 
  **/
 /*Header files that will define level design and objects implementation*/
+typedef double Flt;
 
+class Level {
+    public:
+    unsigned char arr[16][80];
+    int nrows, ncols;
+    int tilesize[2];
+    Flt ftsz[2];
+    Flt tile_base;
+    Level();
+    void removeCrLf(char *str);
+};
 #endif
