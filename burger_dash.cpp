504af4edc15c76d284268706675eb53689e006d9
JesusAstro <115951299+JesusAstro@users.noreply.github.com>
2023-10-20 21:06:17 -0700

    Update burger_dash.cpp
    update to render level

--- burger_dash.cpp
+++ burger_dash.cpp
@@ -24,6 +24,10 @@ bool startScreenActive = true;
 #define rnd() (float)rand() / (float)RAND_MAX
 
 const float GRAVITY = 1.0f;
+//Camer for level render
+double camera[2] = {0.0,0.0};
+//Declare global Level class
+Level lev;
 
 class Global {
     public:
@@ -444,6 +448,61 @@ void render()
     glVertex2f( burger.width, -burger.height);
     glEnd();
     glPopMatrix();
+
+    //Render LLevel
+    //========================
+    int tx = lev.tilesize[0];
+    int ty = lev.tilesize[1];
+    Flt dd = lev.ftsz[0];
+    Flt offy = lev.tile_base;
+    int ncols_to_render = gl.xres / lev.tilesize[0] + 2;
+    int col = (int)(camera[0] / dd);
+    col = col % lev.ncols;
+    //Partial tile offset must be determined here.
+    //The leftmost tile might be partially off-screen.
+    //cdd: camera position in terms of tiles.
+    Flt cdd = camera[0] / dd;
+    //flo: just the integer portion
+    Flt flo = floor(cdd);
+    //dec: just the decimal portion
+    Flt dec = (cdd - flo);
+    //offx: the offset to the left of the screen to start drawing tiles
+    Flt offx = -dec * dd;
+    //Log("gl.camera[0]: %lf   offx: %lf\n",gl.camera[0],offx);
+    for (int j=0; j<ncols_to_render; j++) {
+    int row = lev.nrows-1;
+    for (int i=0; i<lev.nrows; i++) {
+        if (lev.arr[row][col] == 'w') {
+        glColor3f(0.8, 0.8, 0.6);
+        glPushMatrix();
+        //put tile in its place
+        glTranslated((Flt)j*dd+offx, (Flt)i*lev.ftsz[1]+offy, 0);
+        glBegin(GL_QUADS);
+        glVertex2i( 0,  0);
+        glVertex2i( 0, ty);
+        glVertex2i(tx, ty);
+        glVertex2i(tx,  0);
+        glEnd();
+        glPopMatrix();
+        }
+        if (lev.arr[row][col] == 'b') {
+        glColor3f(0.9, 0.2, 0.2);
+        glPushMatrix();
+        glTranslated((Flt)j*dd+offx, (Flt)i*lev.ftsz[1]+offy, 0);
+        glBegin(GL_QUADS);
+        glVertex2i( 0,  0);
+        glVertex2i( 0, ty);
+        glVertex2i(tx, ty);
+        glVertex2i(tx,  0);
+        glEnd();
+        glPopMatrix();
+        }
+        --row;
+    }
+    col = (col+1) % lev.ncols;
+    }
+    glColor3f(1.0, 1.0, 0.1);
+    glPushMatrix();
 
     //draw spike
     glPushMatrix();
