b31072770ea7f2b607645dd03b7025293cdf2f81
Mario Jimenez <118569104+mjimenez818@users.noreply.github.com>
2023-10-24 08:43:33 -0700

    Update mjimenez.h

--- mjimenez.h
+++ mjimenez.h
@@ -1,10 +1,11 @@
 #ifndef _MARIO_H_
 #define _MARIO_H_
-
+#include <time.h>
 // Mario's project files
 
 // called from main program
 //extern void display_name(int, int);
 extern void display_border(int xres, int yres);
+extern int time_since_key_press(time_t key_checker);
 
 #endif
