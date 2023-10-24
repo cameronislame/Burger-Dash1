d28582c460eb50ae0591c8970b0764b185b2523c
Cameron McDaniel <143523058+cameronislame@users.noreply.github.com>
2023-10-24 08:06:36 -0700

    Update cmcdaniel.h
    included total_running_time(time_t begin);

--- cmcdaniel.h
+++ cmcdaniel.h
@@ -1,13 +1,13 @@
 #ifndef _Cameron_M_
 #define _Cameron_M_
-
+#include <time.h>
 //Cameron's project files
 
 
 //extern void display_name(int, int);
 //extern void display_border(int xres, int yres);
 extern void display_credits(int xres, int yres);
-
+extern int total_running_time(time_t begin);
 class cameron{
 	int abc;
 };
