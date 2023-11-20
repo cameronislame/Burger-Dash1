b148581a3313a71e4252e8d6b6a34cb1e11d2236
Mario Jimenez <118569104+mjimenez818@users.noreply.github.com>
2023-11-20 01:15:27 -0800

    moving render start screen

--- mjimenez.cpp
+++ mjimenez.cpp
@@ -7,25 +7,49 @@
 
 #include "mjimenez.h"
 #include "fonts.h"
-#include <GL/glx.h>
+#include "X11_wrapper.h"
 #include <time.h>
-/*
-int time_since_key_press(const bool get) {
-    static int check_keys = 1;
-    static int start_time;
-    if (check_keys) {
-        start_time = time(NULL);
-        check_keys = 0;
-    }
-    if (get) {
-        return time(NULL) - start_time;
-    }
-    return 0;
-}
-*/
+#include <GL/glx.h>
 
 int time_since_key_press(time_t begin) {
         return time(NULL) - begin;
+}
+
+void renderStartScreen(int xres, int yres, X11_wrapper& x11, Global& gl) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Calculate the center of the screen
+    int centerX = xres / 2;
+    int centerY = yres / 2;
+    // Calculate the position of the yellow square relative to the center
+    int squareWidth = 800; // Increase the size as needed
+    int squareHeight = 300;
+    int squareX = centerX - (squareWidth / 2);
+    int squareY = centerY - (squareHeight / 2);
+    // Draw the larger yellow square in the center
+    glColor3ub(255, 255, 0);
+    glPushMatrix();
+    glTranslatef(squareX, squareY, 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(0, 0);
+    glVertex2f(0, squareHeight);
+    glVertex2f(squareWidth, squareHeight);
+    glVertex2f(squareWidth, 0);
+    glEnd();
+    glPopMatrix();
+    // Text Color
+    unsigned int c = 0xf0f00044; // Color
+    Rect r;
+    r.bot = squareY + (squareHeight / 2); // Center vertically
+    r.left = squareX + (squareWidth / 2); // Center horizontally
+    r.center = 1;
+    ggprint8b(&r, 16, c, "Burger Dash");
+
+    r.bot = squareY - 20;
+    r.left = centerX;
+    r.center = 1;
+    ggprint8b(&r, 16, c, "Press Enter to Start");
+
+    x11.swapBuffers();
 }
 
 void display_border(int xres, int yres)
@@ -52,6 +76,7 @@ void display_border(int xres, int yres)
     glPopMatrix();
     glDisable(GL_BLEND);
 }
+
 /*
 void display_name(int x, int y)
 {
@@ -61,5 +86,20 @@ void display_name(int x, int y)
     r.center = 0;
     ggprint8b(&r, 0, 0x00000000, "Mario");
 
+}
+*/
+
+/*
+int time_since_key_press(const bool get) {
+    static int check_keys = 1;
+    static int start_time;
+    if (check_keys) {
+        start_time = time(NULL);
+        check_keys = 0;
+    }
+    if (get) {
+        return time(NULL) - start_time;
+    }
+    return 0;
 }
 */
