61dd5c0d797bc16c2be5ee868425c0665efc28ff
JesusAstro <115951299+JesusAstro@users.noreply.github.com>
2023-12-03 17:30:36 -0800

    Update jbaltazarrob.cpp
    code for rendering health pack and knives.

--- jbaltazarrob.cpp
+++ jbaltazarrob.cpp
@@ -1,4 +1,7 @@
 #include "jbaltazarrob.h"
+#include <GL/glx.h>
+#include "cmcdaniel.h"
+#include "cestes.h"
 #include <iostream>
 #include <cstddef>
 #include <cstring>
@@ -12,6 +15,10 @@
  * CODE COMES FROM GORDON WALK2*/
 float render_val_count = 0;
 Square hp_pack;
+//Knife obj 
+Square knife1;
+Square knife2;
+Square knife3;
 Level::Level() {
     //Log("Level constructor\n");
     tilesize[0] = 8;
@@ -94,10 +101,86 @@ float total_render_function_calls(){
 }
 void init_hpPack()
 {
-    hp_pack.pos[0] = 1500.0;
-    hp_pack.pos[1] = 200;
+    //hp_pack.pos[0] = gl.xres + 300.0;
+    hp_pack.pos[0] = burger.pos[0] + (float)gl.xres  + 300.0; 
+    hp_pack.pos[1] = gl.yres / 3.0;
     hp_pack.width = 20.0;
-    hp_pack.vel[0] = -50;
+    hp_pack.vel[0] = -30;
     hp_pack.height = 20.0;
 
+}
+void initKnives()
+{
+    //initialize knives in the air
+    knife1.pos[0] = gl.xres + knife1.width;
+    knife1.pos[1] = gl.yres / 4.0;
+    knife1.width = 20.0;
+    knife1.vel[0] = -40;
+    knife1.height = 4.0;
+
+
+    knife2.pos[0] = gl.xres + knife2.width + 20.0;
+    knife2.pos[1] = (gl.yres / 4.0) + 40.0;
+    knife2.width = 20.0;
+    knife2.vel[0] = -40;
+    knife2.height = 4.0;
+
+    knife3.pos[0] = gl.xres + knife3.width + 20;
+    knife3.pos[1] = (gl.yres / 4.0) - 40.0;
+    knife3.width = 20.0;
+    knife3.vel[0] = -40;
+    knife3.height = 4.0;
+}
+void render_knives()
+{
+    //Knives in the air
+    glPushMatrix();
+    glColor3ub(160,32,240);
+    glTranslatef(knife1.pos[0], knife1.pos[1], 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-knife1.width, -knife1.height);
+    glVertex2f(-knife1.width,  knife1.height);
+    glVertex2f( knife1.width,  knife1.height);
+    glVertex2f( knife1.width, -knife1.height);
+    glEnd();
+    glPopMatrix();
+
+
+    glPushMatrix();
+    glColor3ub(160,32,240);
+    glTranslatef(knife2.pos[0], knife2.pos[1], 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-knife2.width, -knife2.height);
+    glVertex2f(-knife2.width,  knife2.height);
+    glVertex2f( knife2.width,  knife2.height);
+    glVertex2f( knife2.width, -knife2.height);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    glColor3ub(160,32,240);
+    glTranslatef(knife3.pos[0], knife3.pos[1], 0.0f);
+    glBegin(GL_QUADS);
+    glVertex2f(-knife3.width, -knife3.height);
+    glVertex2f(-knife3.width,  knife3.height);
+    glVertex2f( knife3.width,  knife3.height);
+    glVertex2f( knife3.width, -knife3.height);
+    glEnd();
+    glPopMatrix();
+}
+void render_hp_pack()
+{
+    //render hp pack in the air
+    if(healthbar.health < 170) {
+        glPushMatrix();
+        glColor3ub(0,255,0);
+        glTranslatef(hp_pack.pos[0], hp_pack.pos[1], 0.0f);
+        glBegin(GL_QUADS);
+        glVertex2f(-hp_pack.width, -hp_pack.height);
+        glVertex2f(-hp_pack.width,  hp_pack.height);
+        glVertex2f( hp_pack.width,  hp_pack.height);
+        glVertex2f( hp_pack.width, -hp_pack.height);
+        glEnd();
+        glPopMatrix();
+    }
 }
