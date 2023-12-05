64a96b464c5631c7fee5b7635dd745eee2aad8e3
Mario Jimenez <118569104+mjimenez818@users.noreply.github.com>
2023-12-05 00:08:01 -0800

    Re-Added Shield

--- mjimenez.h
+++ mjimenez.h
@@ -6,9 +6,31 @@
  * File: mjimenez.h
  * Description: Mario Jimenez Header File 
  **/
+#include "cestes.h"
 #include <time.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "X11_wrapper.h"
 class Global;
+
+class ShieldPowerUp {
+public:
+    float pos[2];
+    float vel[2];
+    float width;
+    float height;
+    bool activated;
+    bool active;
+    time_t activationTime;
+    void activate();
+    bool isActivated();
+    ShieldPowerUp() {
+        init();
+    }
+    void init();
+
+};
+bool Check4(Square burger, ShieldPowerUp shieldPowerUp);
 // called from main program
 //extern void display_name(int, int);
 extern void renderStartScreen(int xres, int yres, X11_wrapper& x11, Global& gl);
